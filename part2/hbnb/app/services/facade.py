ca8a07722466e0a21e80a1cdb8b151776bc86279
Handroc <andricassani42069@gmail.com>
2026-02-25 13:04:19 +0100

    fix: proper code from feature branches merged

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,5 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.amenity import Amenity
+from app.models.place import Place
+
 
 class HBnBFacade:
     def __init__(self):
@@ -36,3 +38,53 @@ class HBnBFacade:
 
         amenity.update(amenity_data)
         return amenity
+
+    def create_place(self, place_data):
+        place_data = dict(place_data)
+        owner_id = place_data.pop("owner_id")
+        amenities_ids = place_data.pop("amenities", [])
+
+        owner = self.user_repo.get(owner_id)
+        if not owner:
+            raise ValueError("Owner not found")
+
+        place = Place(owner=owner, **place_data)
+
+        for id in amenities_ids:
+            amenity = self.amenity_repo.get(id)
+            if not amenity:
+                raise ValueError("Amenity not found")
+            place.add_amenity(amenity)
+
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place_data = dict(place_data)
+        place = self.place_repo.get(place_id)
+        if not place:
+            raise ValueError("Place not found")
+
+        if "owner_id" in place_data:
+            owner = self.user_repo.get(place_data.pop("owner_id"))
+            if not owner:
+                raise ValueError("Owner not found")
+            place.owner = owner
+
+        if "amenities" in place_data:
+            amenities_ids = place_data.pop("amenities")
+            place.amenities.clear()
+            for amenity_id in amenities_ids:
+                amenity = self.amenity_repo.get(amenity_id)
+                if not amenity:
+                    raise ValueError("Amenity not found")
+                place.add_amenity(amenity)
+
+        place.update(place_data)
+        return place
